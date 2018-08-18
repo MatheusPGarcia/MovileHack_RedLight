17044b4e1d12ffabdd8a422b01998f1bfa0eb4d3
Unknown <matheuspgarcia@gmail.com>
2018-08-18 20:58:41 -0300

    Adicionando pods

---
+++ iOS/RedLight/RedLight/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  RedLight
+//
+//  Created by Matheus Garcia on 18/08/18.
+//  Copyright © 2018 Matheus Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
