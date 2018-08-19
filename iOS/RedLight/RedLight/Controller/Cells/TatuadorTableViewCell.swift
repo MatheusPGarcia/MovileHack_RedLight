aaef961d1aeda944bfe5730bfcf11b39ed6f823b
Unknown <matheuspgarcia@gmail.com>
2018-08-19 10:29:37 -0300

    Adicionando chat

---
+++ iOS/RedLight/RedLight/Controller/Cells/TatuadorTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TatuadorTableViewCell.swift
+//  RedLight
+//
+//  Created by Matheus Garcia on 19/08/18.
+//  Copyright © 2018 Matheus Garcia. All rights reserved.
+//
+
+import UIKit
+
+class TatuadorTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var messageContent: UILabel!
+    @IBOutlet weak var avatar: UIImageView!
+
+    override func layoutSubviews() {
+        avatar.backgroundColor = .clear
+        avatar.layer.borderWidth = 2
+        avatar.layer.borderColor = UIColor.white.cgColor
+
+        avatar.layer.cornerRadius = avatar.frame.width / 2
+        avatar.layer.masksToBounds = true
+    }
+}
